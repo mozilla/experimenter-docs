4ca7b47ff108e860f3e4ad93148138246c000fc9
Kate Hudson <k88hudson@gmail.com>
2021-03-25 10:19:52 -0400

    Add design process (#19)

--- sidebars.js
+++ sidebars.js
@@ -7,37 +7,20 @@ module.exports = {
     },
     {
       type: "category",
-      label: "Process",
+      label: "How to Run Experiments",
       items: [
+        "process-getting-started",
+        {
+          type: "link",
+          label: "Design Process",
+          href:
+            "https://mana.mozilla.org/wiki/display/FIREFOX/Experiment+Design+Process",
+        },
         {
           type: "link",
           label: "Nimbus Onboarding",
           href:
             "https://mana.mozilla.org/wiki/display/FJT/Nimbus+Onboarding#NimbusOnboarding-Branches",
-        },
-        {
-          type: "link",
-          label: "How to Launch a Nimbus Experiment",
-          href:
-            "https://mana.mozilla.org/wiki/display/FJT/How+to+Launch+a+Nimbus+Experiment",
-        },
-        {
-          type: "link",
-          label: "Nimbus Data Transfer Object",
-          href:
-            "https://mana.mozilla.org/wiki/pages/viewpage.action?pageId=130920248",
-        },
-        {
-          type: "link",
-          label: "Experiment Telemetry",
-          href:
-            "https://mana.mozilla.org/wiki/display/FJT/Nimbus+Engineering#NimbusEngineering-ExperimentTelemetry",
-        },
-        {
-          type: "link",
-          label: "Lessons Learned",
-          href:
-            "https://mana.mozilla.org/wiki/display/FJT/Lessons+Learned+-+Experiment+Incidents+and+Close+Calls",
         },
       ],
     },
@@ -63,18 +46,7 @@ module.exports = {
       label: "Data Scientists",
       items: ["data-scientists-root"],
     },
-    {
-      type: "category",
-      label: "Experimenter Engineers",
-      items: [
-        "experimenter-engineers-root",
-        {
-          type: "link",
-          label: "GitHub Repo",
-          href: "https://github.com/mozilla/experimenter",
-        },
-      ],
-    },
+
     {
       type: "category",
       label: "Fenix Engineers",
@@ -95,6 +67,42 @@ module.exports = {
         "desktop-engineers-root",
         "migration-guide-desktop-frontend",
         "desktop-frontend-testing",
+      ],
+    },
+    {
+      type: "category",
+      label: "Core Nimbus Team",
+      items: [
+        "experimenter-engineers-root",
+        {
+          type: "link",
+          label: "GitHub Repo",
+          href: "https://github.com/mozilla/experimenter",
+        },
+        {
+          type: "link",
+          label: "How to Launch a Nimbus Experiment",
+          href:
+            "https://mana.mozilla.org/wiki/display/FJT/How+to+Launch+a+Nimbus+Experiment",
+        },
+        {
+          type: "link",
+          label: "Nimbus Data Transfer Object",
+          href:
+            "https://mana.mozilla.org/wiki/pages/viewpage.action?pageId=130920248",
+        },
+        {
+          type: "link",
+          label: "Experiment Telemetry",
+          href:
+            "https://mana.mozilla.org/wiki/display/FJT/Nimbus+Engineering#NimbusEngineering-ExperimentTelemetry",
+        },
+        {
+          type: "link",
+          label: "Lessons Learned",
+          href:
+            "https://mana.mozilla.org/wiki/display/FJT/Lessons+Learned+-+Experiment+Incidents+and+Close+Calls",
+        },
       ],
     },
   ],
