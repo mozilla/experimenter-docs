c27e17df15a57355c86f55dcb68b45b9cb798ed8
Andrei Oprea <andrei.br92@gmail.com>
2021-07-30 17:46:51 +0200

    Add page about Platform usage for Feature API

--- sidebars.js
+++ sidebars.js
@@ -112,6 +112,7 @@ module.exports = {
       label: "Desktop Engineers",
       items: [
         "desktop-feature-api",
+        "platform-feature-api",
         "desktop-migration-guide",
         "desktop-rollouts",
         "desktop-frontend-testing",
