9aca05218b094bd8f6094e2924f82446609de813
Meg Viar <lmegviar@gmail.com>
2022-02-13 10:48:55 -0500

    Add 'Available Targeting' link to Messaging in sidebar

--- sidebars.js
+++ sidebars.js
@@ -155,6 +155,11 @@ module.exports = {
           label: "Triggers & User Actions",
           href: "https://firefox-source-docs.mozilla.org/toolkit/components/messaging-system/docs/",
         },
+        {
+          type: "link",
+          label: "Available Targeting",
+          href: "https://firefox-source-docs.mozilla.org/browser/components/newtab/content-src/asrouter/docs/targeting-attributes.html",
+        }
       ],
     },
   ],
